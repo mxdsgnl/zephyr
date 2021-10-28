cdf0d75b010f27de1d06d51d1fde8ec41d09d1ab
mxdsgnl <mxdsgnl.junk@gmail.com>
2021-10-28 11:32:29 -0600

    initial commit of blackparrot port. builds hello_world sample

--- soc/riscv/riscv-privilege/blackparrot/soc.h
+++ soc/riscv/riscv-privilege/blackparrot/soc.h
@@ -4,13 +4,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef __RISCV_VIRT_SOC_H_
-#define __RISCV_VIRT_SOC_H_
+#ifndef __RISCV_BLACKPARROT_H_
+#define __RISCV_BLACKPARROT_H_
 
 #include <soc_common.h>
 #include <devicetree.h>
 
-#define SIFIVE_SYSCON_TEST           0x00100000
 #define RISCV_MTIME_BASE             0x0030BFF8
 #define RISCV_MTIMECMP_BASE          0x00304000
 
